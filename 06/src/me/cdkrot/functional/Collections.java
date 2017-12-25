d44dd51949ebfe473d7b769795ebd9a12c9152b9
Dmitry Sayutin <cdkrot@yandex.ru>
2017-12-26 01:48:27 +0300

    fix generics & lists

--- 06/src/me/cdkrot/functional/Collections.java
+++ 06/src/me/cdkrot/functional/Collections.java
@@ -1,19 +1,23 @@
 package me.cdkrot.functional;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Iterator;
 
+/**
+ * Provides utilities for applying functions to collections.
+ */
 public class Collections {
     /**
      * Maps function to each element of the collection.
-     * @param f -- the transforming function
-     * @param iter -- the collection.
-     * @param <A> -- the collection type
-     * @param <B> -- the result type.
+     * @param f the transforming function
+     * @param iter the collection.
+     * @param <A> the collection type
+     * @param <B> the result type.
      * @return list.
      */
-    public static <A, B> ArrayList<B> map(Function1<A, B> f, Iterable<A> iter) {
-        ArrayList<B> result = new ArrayList<B>();
+    public static <A, B> List<B> map(Function1<? super A, B> f, Iterable<A> iter) {
+        List<B> result = new ArrayList<B>();
 
         for (A a: iter)
             result.add(f.apply(a));
@@ -22,13 +26,13 @@ public class Collections {
 
     /**
      * Filters elements from collection based on f
-     * @param f -- filtering function
-     * @param iter -- the collection
-     * @param <A> -- the type of collection.
+     * @param f filtering function
+     * @param iter the collection
+     * @param <A> the type of collection.
      * @return the list with elements with f(x) = true.
      */
-    public static <A> ArrayList<A> filter(Predicate<A> f, Iterable<A> iter) {
-        ArrayList<A> result = new ArrayList<A>();
+    public static <A> List<A> filter(Predicate<? super A> f, Iterable<A> iter) {
+        List<A> result = new ArrayList<A>();
 
         for (A a: iter)
             if (f.apply(a))
@@ -38,13 +42,13 @@ public class Collections {
 
     /**
      * Takes largest prefix of collection, on which f returns true.
-     * @param f -- the function
-     * @param iter -- the collection
-     * @param <A> -- the type of collection.
+     * @param f the function
+     * @param iter the collection
+     * @param <A> the type of collection.
      * @return the prefix.
      */
-    public static <A> ArrayList<A> takeWhile(Predicate<A> f, Iterable<A> iter) {
-        ArrayList<A> result = new ArrayList<A>();
+    public static <A> List<A> takeWhile(Predicate<? super A> f, Iterable<A> iter) {
+        List<A> result = new ArrayList<A>();
 
         for (A a: iter)
             if (!f.apply(a))
@@ -56,41 +60,31 @@ public class Collections {
 
     /**
      * Takes largest prefix of collection, on which f returns false.
-     * @param f -- the function
-     * @param iter -- the collection
-     * @param <A> -- the type of collection.
+     * @param f the function
+     * @param iter the collection
+     * @param <A> the type of collection.
      * @return the prefix.
      */
-    public static <A> ArrayList<A> takeUnless(Predicate<A> f, Iterable<A> iter) {
-        // one could implement it through takeWhile, but only if he doesn't care
-        // about efficiency.
-
-        ArrayList<A> result = new ArrayList<A>();
-
-        for (A a: iter)
-            if (f.apply(a))
-                break;
-            else
-                result.add(a);
-        return result;
+    public static <A> List<A> takeUnless(Predicate<? super A> f, Iterable<A> iter) {
+        return takeWhile(f.not(), iter);
     }
 
     /**
      * Folds the list into one value.
-     * @param f -- the foldering function
-     * @param value -- initial value
-     * @param iter -- the collection
-     * @param <A> -- the collection type
-     * @param <B> -- the result type
+     * @param f the foldering function
+     * @param value initial value
+     * @param iter the collection
+     * @param <A> the collection type
+     * @param <B> the result type
      * @return the folding result
      */
-    public static <A, B> B foldl(Function2<B, A, B> f, B value, Iterable<A> iter) {
+    public static <A, B> B foldl(Function2<? super B, ? super A, B> f, B value, Iterable<A> iter) {
         for (A a: iter)
             value = f.apply(value, a);
         return value;
     }
 
-    private static <A, B> B foldrImpl(Function2<A, B, B> f, B value, Iterator<A> iter) {
+    private static <A, B> B foldrImpl(Function2<? super A, ? super B, B> f, B value, Iterator<A> iter) {
         if (!iter.hasNext())
             return value;
 
@@ -100,14 +94,14 @@ public class Collections {
 
     /**
      * Folds the list into one value.
-     * @param f -- the foldering function
-     * @param value -- initial value
-     * @param iter -- the collection
-     * @param <A> -- the collection type
-     * @param <B> -- the result type
+     * @param f the foldering function
+     * @param value initial value
+     * @param iter the collection
+     * @param <A> the collection type
+     * @param <B> the result type
      * @return the folding result
      */
-    public static <A, B> B foldr(Function2<A, B, B> f, B value, Iterable<A> iter) {
+    public static <A, B> B foldr(Function2<? super A, ? super B, B> f, B value, Iterable<A> iter) {
         return foldrImpl(f, value, iter.iterator());
     }
 }
